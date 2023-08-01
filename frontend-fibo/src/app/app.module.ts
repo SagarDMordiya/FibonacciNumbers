f31333a7e8c0dadef8d4121d5999e946b5b21525
SagarDMordiya <Mordiya.s@northeastern.edu>
2023-08-01 16:05:03 -0400

    Update the main module
    Add relevant module and removed the irrelevant code from html

--- frontend-fibo/src/app/app.module.ts
+++ frontend-fibo/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { OutputComponent } from './output/output.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
