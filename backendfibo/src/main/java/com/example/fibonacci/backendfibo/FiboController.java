a56ff37338b71a096d0162a428d0c17fbc1f9458
SagarDMordiya <Mordiya.s@northeastern.edu>
2023-08-01 15:58:03 -0400

    Added CORS
    Add CORS to enable cross origin request for http://localhost:4200

--- backendfibo/src/main/java/com/example/fibonacci/backendfibo/FiboController.java
+++ backendfibo/src/main/java/com/example/fibonacci/backendfibo/FiboController.java
@@ -3,12 +3,14 @@ package com.example.fibonacci.backendfibo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/")
 public class FiboController {
 
