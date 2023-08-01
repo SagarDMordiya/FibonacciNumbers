84996ad7c5370449cf52d2a0ce879acb162f8796
SagarDMordiya <Mordiya.s@northeastern.edu>
2023-08-01 10:40:06 -0400

    Created new Java project using Spring Boot

---
+++ backendfibo/src/main/java/com/example/fibonacci/backendfibo/BackendfiboApplication.java
@@ -0,0 +1,13 @@
+package com.example.fibonacci.backendfibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfiboApplication.class, args);
+	}
+
+}
---
+++ backendfibo/src/test/java/com/example/fibonacci/backendfibo/BackendfiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fibonacci.backendfibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
