520c9de707dc70b5848725917a2bd91f11ab41a4
SagarDMordiya <Mordiya.s@northeastern.edu>
2023-08-01 16:00:18 -0400

    API call
    Added new function to utilize API calls and redirect to next page

--- frontend-fibo/src/app/fibo.service.ts
+++ frontend-fibo/src/app/fibo.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
+import { state } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FiboService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private rounter: Router) { }
+  api: String = "http://localhost:8080/"
+
+  getFiboNo(input: any) {
+    console.log(this.api+"fiboNo/"+input);
+    this.httpClient.get(this.api+"fiboNo/"+input).subscribe(
+      (response) => {
+        this.rounter.navigate(['/output'], {state: {response, input}});
+      },
+      (error) =>{
+        throw new Error('Method not implemented.');
+      }
+    );
+    
+  }
+
 }
