8430aeb494d04c53ef499ef56259187eb6004fe6
SagarDMordiya <Mordiya.s@northeastern.edu>
2023-08-01 16:01:37 -0400

    Update input.component.ts
    Added function of Onsubmit to check value and call the service method

--- frontend-fibo/src/app/input/input.component.ts
+++ frontend-fibo/src/app/input/input.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FiboService } from '../fibo.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
+
 export class InputComponent {
+
+  constructor(private fiboService: FiboService) { }
+
+  input!: number;
+  ngOnInit() {
+    console.log("inside this");
+  }
+  onSubmit() {
+    if (Number.isInteger(this.input)) {
+      console.log(this.input)
+      this.fiboService.getFiboNo(this.input);
+    }
+    else {
+      alert("Please enter a Valid Integer");
+    }
+  }
 
 }
